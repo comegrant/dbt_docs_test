8ddf7178b8a4840bceb8202ba44546bcd9f589d3
annaubbroyn <anna.broyn@cheffelo.com>
2024-08-15 08:44:15 +0200

    Using target.database instead of target.name in dbt

--- projects/data-model/transform/macros/generate_schema_name.sql
+++ projects/data-model/transform/macros/generate_schema_name.sql
@@ -1,11 +1,11 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if target.name != 'dev' and custom_schema_name is not none -%}
+    {%- if target.database != 'dev' and custom_schema_name is not none -%}
 
         {{ custom_schema_name | trim }}
     
-    {%- elif target.name == 'dev' and custom_schema_name is not none -%}
+    {%- elif target.database == 'dev' and custom_schema_name is not none -%}
 
         {{ default_schema }}_{{ custom_schema_name | trim }}
     
