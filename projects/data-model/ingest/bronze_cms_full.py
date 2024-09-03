f4c2b9d64e3a81d61232c07265873bd88fbf06f7
Sylvia Liu <102959222+sylvia-liu-qinghua@users.noreply.github.com>
2024-09-03 13:01:29 +0200

    ingest cms.basket and basket deviation to bronze (#112)

--- projects/data-model/ingest/bronze_cms_full.py
+++ projects/data-model/ingest/bronze_cms_full.py
@@ -2,13 +2,13 @@
 
 from coredb_connector import load_coredb_full
 
-
 # COMMAND ----------
 
 database = "CMS"
 tables = [
         "addon_subscriptions",
         "billing_agreement",
+        "billing_agreement_basket",
         "billing_agreement_status",
         "billing_agreement_addon_subscriptions",
         "billing_agreement_order_status",
@@ -18,5 +18,5 @@ tables = [
         "loyalty_level",
         "order_type"
         ]
-for table in tables: 
+for table in tables:
     load_coredb_full(dbutils, database, table)
