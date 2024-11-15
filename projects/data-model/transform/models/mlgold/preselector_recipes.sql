bfbc517415534a40ab08e92a4f8a12bef0567dcc
Marie Borg <72552403+Marieilona@users.noreply.github.com>
2024-11-15 12:13:32 +0100

    preselector_recipe table in mlgold (#368)

---
+++ projects/data-model/transform/models/mlgold/preselector_recipes.sql
@@ -0,0 +1,49 @@
+with recommendations as (
+
+    select * from {{ ref('int_basket_deviation_recommendations_most_recent') }}
+)
+
+, menus as (
+
+    select * from {{ ref('int_weekly_menus_variations_recipes_portions_joined') }}
+
+)
+
+, products as (
+
+    select * from {{ ref('int_product_tables_joined') }}
+
+)
+
+, agreements as (
+
+    select * from {{ref('cms__billing_agreements')}}
+    -- are currently null for the active row, but will be changed to future proof date soon
+    where valid_to is null or valid_to = '{{ var("future_proof_date") }}'
+
+)
+
+, recommendation_engine_preselected_recipes as (
+    select
+        recommendations.menu_week_monday_date
+        , recommendations.menu_week
+        , recommendations.menu_year
+        , recommendations.billing_agreement_basket_id
+        , recommendations.billing_agreement_id
+        , recommendations.product_variation_id
+        , menus.recipe_id
+    from recommendations
+    left join agreements
+        on recommendations.billing_agreement_id = agreements.billing_agreement_id
+    left join menus
+        on recommendations.menu_week_monday_date = menus.menu_week_monday_date
+        and recommendations.product_variation_id = menus.product_variation_id
+        and agreements.company_id = menus.company_id
+    left join products
+        on recommendations.product_variation_id = products.product_variation_id
+        and agreements.company_id = products.company_id
+    where product_type_id = '{{ var("velg&vrak_product_type_id") }}'
+    and recommendations.billing_agreement_basket_deviation_origin_id = '{{ var("preselector_origin_id") }}'
+)
+
+select * from recommendation_engine_preselected_recipes
