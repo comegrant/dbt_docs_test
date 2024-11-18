f0a99598282c3ac11d0491932620df9d1afae2eb
Sylvia Liu <102959222+sylvia-liu-qinghua@users.noreply.github.com>
2024-11-18 16:03:08 +0100

    update model uri in test (#372)

--- projects/dishes-forecasting/dishes_forecasting/predict/configs.py
+++ projects/dishes-forecasting/dishes_forecasting/predict/configs.py
@@ -8,14 +8,14 @@ class PredictConfigs(BaseModel):
 gl_configs = PredictConfigs(
     model_uri={
         "dev": "runs:/172c141f1d984d5c9719bd49c2f0d7f5/dishes_forecasting_GL",
-        "test": "runs:/027d6f026e7f4552b7f68944dc16d8a8/dishes_forecasting_GL",
+        "test": "runs:/dd8d57c749b446d8a336cae94a22ecd7/dishes_forecasting_GL",
     }
 )
 
 amk_configs = PredictConfigs(
     model_uri={
         "dev": "runs:/e6891dfd222342b1b931339a3af6ed4d/dishes_forecasting_AMK",
-        "test": "runs:/541214cc0adb4674afa3e4fbf2c7b2b0/dishes_forecasting_AMK",
+        "test": "runs:/839370005b754de0ad03afd6798c73bd/dishes_forecasting_AMK",
     }
 )
 
@@ -23,14 +23,14 @@ amk_configs = PredictConfigs(
 rt_configs = PredictConfigs(
     model_uri={
         "dev": "runs:/c4ac7d1e087c4b22bbb2eabc32c8ac90/dishes_forecasting_RT",
-        "test": "runs:/010ce28668c44ef49776c0483f7892c8/dishes_forecasting_RT",
+        "test": "runs:/7c9c948cfd0b444b9b6e2b81cb92e0f3/dishes_forecasting_RT",
     }
 )
 
 lmk_configs = PredictConfigs(
     model_uri={
         "dev": "runs:/9d4390ad01c44afebd86383b380c97a4/dishes_forecasting_LMK",
-        "test": "runs:/b1fb66c025d041bda812f5b983c44ccd/dishes_forecasting_LMK",
+        "test": "runs:/7c1c4cebb7a540c98ea83854f10fb3f9/dishes_forecasting_LMK",
     }
 )
 
