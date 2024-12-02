45917e100b2b057264c9348e8aa3c0a67944a164
Marie Borg <72552403+Marieilona@users.noreply.github.com>
2024-12-02 15:39:32 +0100

    add id columns and remove unecessary joins (#439)

--- projects/data-model/transform/models/gold/sales/fact_subscription_quiz.sql
+++ projects/data-model/transform/models/gold/sales/fact_subscription_quiz.sql
@@ -6,24 +6,6 @@ billing_agreements as (
 
 )
 
-, companies as (
-
-    select * from {{ ref('dim_companies') }}
-
-)
-
-, date as (
-
-    select * from {{ ref('dim_date') }}
-
-)
-
-, time as (
-
-    select * from {{ ref('dim_time') }}
-
-)
-
 , int_subscription_step_has_started_subscription_quiz as (
 
     select * from {{ ref('int_subscription_step_has_started_subscription_quiz') }}
@@ -72,11 +54,16 @@ billing_agreements as (
             , update_subscription_events_unioned.source_created_at_segment
         ))                                           as pk_fact_subscription_quiz
 
+        , update_subscription_events_unioned.event_id_segment
+        , update_subscription_events_unioned.billing_agreement_id
+        , update_subscription_events_unioned.company_id
+        , update_subscription_events_unioned.source_created_at_segment
+        
         {# FKS #}
         , billing_agreements.pk_dim_billing_agreements as fk_dim_billing_agreements
-        , companies.pk_dim_companies                 as fk_dim_companies
-        , date.pk_dim_date                           as fk_dim_date_source_created_at_segment
-        , time.pk_dim_time                           as fk_dim_time_source_created_at_segment
+        , md5(update_subscription_events_unioned.company_id) as fk_dim_companies
+        , cast(date_format(update_subscription_events_unioned.source_created_at_segment, 'yyyyMMdd') as int) as fk_dim_date_source_created_at_segment
+        , cast(date_format(update_subscription_events_unioned.source_created_at_segment, 'HHmm') as int) as fk_dim_time_source_created_at_segment
 
         {# NUMBERS #}
         , update_subscription_events_unioned.has_started_subscription_quiz
@@ -89,16 +76,6 @@ billing_agreements as (
             update_subscription_events_unioned.billing_agreement_id = billing_agreements.billing_agreement_id
             and update_subscription_events_unioned.source_created_at_segment >= billing_agreements.valid_from
             and update_subscription_events_unioned.source_created_at_segment < billing_agreements.valid_to
-    left join companies
-        on update_subscription_events_unioned.company_id = companies.company_id
-    left join date
-        on
-            cast(date_format(update_subscription_events_unioned.source_created_at_segment, 'yyyyMMdd') as int)
-            = date.pk_dim_date
-    left join time
-        on
-            cast(date_format(update_subscription_events_unioned.source_created_at_segment, 'HHmm') as int)
-            = time.pk_dim_time
     where billing_agreements.pk_dim_billing_agreements is not null
 )
 
