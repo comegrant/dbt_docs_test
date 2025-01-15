d1c4cbe8fc6127e869888c242d419cc5100d1f5f
Marie Borg <72552403+Marieilona@users.noreply.github.com>
2025-01-15 15:48:01 +0100

    add mlgold table for preselector to know last time a main recipe id was chosen (#532)

---
+++ projects/data-model/transform/models/mlgold/main_recipe_last_menu_year_week.sql
@@ -0,0 +1,66 @@
+with 
+
+deviations as (
+
+    select * from {{ref('int_basket_deviation_products_joined')}}
+
+)
+
+, menus as (
+
+    select * from {{ref('fact_menus')}}
+
+)
+
+, recipes as (
+
+    select * from {{ref('dim_recipes')}}
+)
+
+, deviations_filter_active as (
+
+    select 
+        deviations.*
+    from deviations
+    where is_active_deviation
+    
+)
+
+, deviations_add_fks as (
+
+    select
+        deviations_filter_active.*
+        , md5(concat(product_variation_id, company_id)) as fk_dim_products
+        , cast(date_format(menu_week_monday_date, 'yyyyMMdd') as int) as fk_dim_date
+    from deviations_filter_active
+
+)
+
+, deviations_menus_recipes_joined as (
+
+    select
+        deviations_add_fks.billing_agreement_id
+        , deviations_add_fks.company_id
+        , deviations_add_fks.menu_year * 100 + deviations_add_fks.menu_week as menu_year_week
+        , coalesce(recipes.main_recipe_id, recipes.recipe_id) as main_recipe_id
+    from deviations_add_fks
+    left join menus 
+        on deviations_add_fks.fk_dim_products = menus.fk_dim_products
+        and deviations_add_fks.fk_dim_date = menus.fk_dim_date
+    left join recipes
+        on menus.fk_dim_recipes = recipes.pk_dim_recipes
+
+)
+
+, deviatons_add_last_active (
+
+    select
+        billing_agreement_id
+        , company_id
+        , main_recipe_id
+        , lag(menu_year_week, 1) over (partition by billing_agreement_id, main_recipe_id order by menu_year_week) as previous_menu_year_week
+    from deviations_menus_recipes_joined
+
+) 
+
+select * from deviatons_add_last_active
