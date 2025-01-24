05e5193921253ef07a3be729c1aecaa36be5eb5a
Marie Borg <72552403+Marieilona@users.noreply.github.com>
2025-01-24 13:12:44 +0100

    use financial calendar to find first delivery year/month/quarter/week (#565)

--- projects/data-model/transform/models/intermediate/sales/int_billing_agreements_extract_first_order.sql
+++ projects/data-model/transform/models/intermediate/sales/int_billing_agreements_extract_first_order.sql
@@ -6,11 +6,18 @@ orders as (
     
 )
 
+, calendar as (
+
+    select * from {{ ref('int_dates_with_financial_periods')}}
+
+)
+
 , first_orders as (
     select 
         billing_agreement_id
         , min_by(source_created_at, menu_week_monday_date) as first_order_created_at
         , min(menu_week_monday_date) as first_menu_week_monday_date
+        , min_by(cast(date_format(source_created_at, 'yyyyMMdd') as int), menu_week_monday_date) as datekey
     from orders
     where order_type_id in ({{ var('subscription_order_type_ids') | join(', ') }})
     and order_status_id in ({{ var('finished_order_status_ids') | join(', ') }})
@@ -18,18 +25,19 @@ orders as (
         1 
 )
 
-, cohorts_first_orders as (
+, first_orders_cohorts as (
     select 
         billing_agreement_id
         , first_order_created_at
         , first_menu_week_monday_date
-        , extract('WEEK', first_menu_week_monday_date) as first_menu_week_week
-        , extract('MONTH', first_menu_week_monday_date) as first_menu_week_month
-        , extract('QUARTER', first_menu_week_monday_date) as first_menu_week_quarter
-        , extract('YEAROFWEEK', first_menu_week_monday_date) as first_menu_week_year
+        , calendar.financial_week as first_menu_week_week
+        , calendar.financial_month_name as first_menu_week_month
+        , calendar.financial_quarter as first_menu_week_quarter
+        , calendar.financial_year as first_menu_week_year
     from first_orders
+    left join calendar 
+        on first_orders.datekey = calendar.pk_dim_dates
 
 )
 
-
-select * from cohorts_first_orders
+select * from first_orders_cohorts
