9490418d2cbf79e4dd6da21879d9092c3c75c1a5
Stephen Allwright <stephenallwright@gmail.com>
2025-02-08 13:20:31 +0100

    Reordered adding versions in silver for preselector output (#621)

--- projects/data-model/transform/models/silver/mloutputs/mloutputs__preselector_successful_realtime_output.sql
+++ projects/data-model/transform/models/silver/mloutputs/mloutputs__preselector_successful_realtime_output.sql
@@ -4,25 +4,10 @@ with preselector_successful_output as (
 
 )
 
-, add_output_version as (
-    select *
-    , row_number() over (
-        partition by billing_agreement_id, menu_week, menu_year
-        order by generated_at asc
-    ) as menu_week_output_version
-    , case
-    when 1 = row_number() over (
-        partition by billing_agreement_id, menu_week, menu_year
-        order by generated_at desc
-    ) then 1
-    else 0
-    end as is_latest_menu_week_output_version
-    from preselector_successful_output
-)
+-- Deplucating due to bug in the output, see issue data-536
+, rename_and_distinct as (
 
-, renamed as (
-
-    select
+    select distinct
         billing_agreement_id,
         company_id,
         concept_preference_ids,
@@ -77,74 +62,25 @@ with preselector_successful_output as (
         coalesce(error_vector.mean_veg_fruit, 0) as error_mean_veg_fruit,
         coalesce(error_vector.repeated_proteins_percentage, 0) as error_repeated_proteins_percentage,
         coalesce(error_vector.repeated_carbo_percentage, 0) as error_repeated_carbohydrates_percentage,
-        menu_week_output_version,
-        is_latest_menu_week_output_version,
         generated_at as created_at
-    from add_output_version
+    from preselector_successful_output
 
 )
 
--- Deplucating due to bug in the output, see issue data-536
-, deduplicated as (
-    select distinct
-        billing_agreement_id
-        , company_id
-        , menu_week
-        , menu_year
-        , menu_week_monday_date
-        , model_version_commit_sha
-        , concept_preference_ids
-        , taste_preference_ids
-        , product_variation_ids
-        , main_recipe_ids
-        , portions
-        , meals
-        , target_cost_of_food_per_meal
-        , has_data_processing_consent
-        , is_override_deviation
-        , taste_preference_compliancy_code
-        , error_cooking_time_mean
-        , error_is_beef_percentage
-        , error_is_chef_choice_percentage
-        , error_is_chicken_percentage
-        , error_is_cod_percentage
-        , error_is_family_friendly_percentage
-        , error_is_gluten_free_percentage
-        , error_is_grain_percentage
-        , error_is_lactose_percentage
-        , error_is_lamb_percentage
-        , error_is_low_calorie
-        , error_is_mixed_meat_percentage
-        , error_is_other_carbo_percentage
-        , error_is_other_protein_percentage
-        , error_is_pasta_percentage
-        , error_is_pork_percentage
-        , error_is_roede_percentage
-        , error_is_salmon_percentage
-        , error_is_seafood_percentage
-        , error_is_shrimp_percentage
-        , error_is_soft_bread_percentage
-        , error_is_spicy_percentage
-        , error_is_tuna_percentage
-        , error_is_vegan_percentage
-        , error_is_vegetables_percentage
-        , error_is_vegetarian_percentage
-        , error_mean_cost_of_food
-        , error_mean_energy
-        , error_mean_fat
-        , error_mean_fat_saturated
-        , error_mean_number_of_ratings
-        , error_mean_ordered_ago
-        , error_mean_protein
-        , error_mean_rank
-        , error_mean_ratings
-        , error_mean_veg_fruit
-        , error_repeated_proteins_percentage
-        , error_repeated_carbohydrates_percentage
-        , menu_week_output_version
-        , is_latest_menu_week_output_version
-        , created_at
-    from renamed
+, add_output_version as (
+    select *
+    , row_number() over (
+        partition by billing_agreement_id, menu_week, menu_year
+        order by created_at asc
+    ) as menu_week_output_version
+    , case
+    when 1 = row_number() over (
+        partition by billing_agreement_id, menu_week, menu_year
+        order by created_at desc
+    ) then 1
+    else 0
+    end as is_latest_menu_week_output_version
+    from rename_and_distinct
 )
 
-select * from deduplicated
+select * from add_output_version
