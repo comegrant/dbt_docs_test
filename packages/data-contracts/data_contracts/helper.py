143421bbbf1fb3c95dffb0e4dd60a17a9516ed8d
Niladri Banerjee, PhD <7581777+Niladri-B@users.noreply.github.com>
2025-02-10 09:49:17 +0100

    Nb/feat/extend helper case conversions (#628)

--- packages/data-contracts/data_contracts/helper.py
+++ packages/data-contracts/data_contracts/helper.py
@@ -1,4 +1,4 @@
-""" A compendium of handy helper functions """
+"""A compendium of handy helper functions for case conversions."""
 
 
 def camel_to_snake(s: str) -> str:
@@ -13,8 +13,28 @@ def camel_to_snake(s: str) -> str:
 
     >>> camel_to_snake('camelCase')
     'camel_case'
+    >>> camel_to_snake('PascalCase')
+    'pascal_case'
     """
     return "".join(["_" + c.lower() if c.isupper() else c for c in s]).lstrip("_")
+
+
+def capital_to_snake(s: str) -> str:
+    """
+    Convert CAPITALCASE string to snake_case
+
+    Args:
+        s (str): The CAPITALCASE string to convert
+
+    Returns:
+        str: The snake_case converted string
+
+    >>> capital_to_snake('CAPITAL_CASE')
+    'capital_case'
+    >>> capital_to_snake('ALL_CAPITAL_CASE')
+    'all_capital_case'
+    """
+    return "_".join(part.lower() for part in s.split("_"))
 
 
 def snake_to_camel(s: str) -> str:
@@ -30,10 +50,18 @@ def snake_to_camel(s: str) -> str:
     Example:
         >>> snake_to_camel('snake_case_string')
         'snakeCaseString'
+        >>> snake_to_camel('Capital_Case')
+        'capitalCase'
+        >>> snake_to_camel('Camel_case')
+        'camelCase'
+        >>> snake_to_camel('ALL_CAPITAL_CASE')
+        'allCapitalCase'
+        >>> snake_to_camel('CAPS')
+        'caps'
     """
-    words = s.split("_")
-    # Capitalize all words except the first one
-    return words[0] + "".join(word.capitalize() for word in words[1:] if word)
+    words = s.lower().split("_")
+    # Capitalize all words except the first one and if first is capitalised, change it to lower
+    return words[0][0].lower() + words[0][1:] + "".join(word.capitalize() for word in words[1:] if word)
 
 
 def snake_to_pascal(s: str) -> str:
@@ -46,8 +74,17 @@ def snake_to_pascal(s: str) -> str:
     Returns:
         str: The converted PascalCase string
 
-    >>> snake_to_pascal('camel_case')
-    'CamelCase'
+    >>> snake_to_pascal('pascal_case')
+    'PascalCase'
+    >>> snake_to_pascal('PASCAL_CAPITAL_CASE')
+    'PascalCapitalCase'
     """
     # Capitalize the first letter of all words
-    return "".join([s[0].upper() + s[1:] if s else s for s in s.split("_")])
+    # and lower the rest of the letters
+    return "".join([s[0].upper() + s[1:].lower() if s else s for s in s.split("_")])
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
