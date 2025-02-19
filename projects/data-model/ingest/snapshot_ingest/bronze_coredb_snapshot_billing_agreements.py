d3bd4788f0f96213ce5e26a316991bfe6f3644bf
Marie Borg <72552403+Marieilona@users.noreply.github.com>
2025-02-19 11:31:43 +0100

    Marie/temp fix agreements ingest (#660)

---
+++ projects/data-model/ingest/snapshot_ingest/bronze_coredb_snapshot_billing_agreements.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC Used to perform additional ingest of billing_agreements outside of the snapshot_full notebook
+
+import sys
+sys.path.append('../helper_functions')
+
+from coredb_connector import load_coredb_full
+
+# COMMAND ----------
+
+load_coredb_full(dbutils, "CMS", "billing_agreement")
