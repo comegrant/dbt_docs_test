512826b9c355765f0f516bf7db110ecec7c9f90d
annaubbroyn <anna.broyn@cheffelo.com>
2025-06-25 14:08:30 +0200

    Filtering fact_billing_agreements_daily in view instead (#1137)

---
+++ projects/data-model/transform/models/gold/sales/view_fact_billing_agreements.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with
+
+all_days as (
+
+    select * from {{ ref('fact_billing_agreements_daily') }}
+
+)
+
+, last_30_days_or_mondays as (
+
+    select 
+    * 
+    from all_days
+    where 
+        date >= current_date() - interval '30 days'
+        or is_monday = true
+
+)
+
+select * from last_30_days_or_mondays
