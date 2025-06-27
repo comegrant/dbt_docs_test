10b5e22afa25ec0a523e1580353331fe24b538a3
linasjolin <lina.sjolin@cheffelo.com>
2025-06-27 14:20:59 +0200

    Create int_reactivation_customer_cohorts (#1149)
    Co-authored-by: annaubbroyn <anna.broyn@cheffelo.com>

---
+++ projects/data-model/transform/models/intermediate/sales/int_reactivation_customer_cohorts.sql
@@ -0,0 +1,25 @@
+with 
+customer_journey_segments as (
+    select * from {{ ref('int_customer_journey_segments') }}
+
+)
+
+, customer_journey_reactivations as (
+    select billing_agreement_id
+        , menu_week_monday_date_from
+    from customer_journey_segments
+    where sub_segment_id = {{ var("customer_journey_sub_segment_ids")["reactivated"] }}
+
+)
+
+, reactivations_with_valid_from_and_to as (
+
+    select
+        billing_agreement_id
+        , menu_week_monday_date_from as menu_week_monday_date_from
+        , {{ get_scd_valid_to('menu_week_monday_date_from ', 'billing_agreement_id') }} as menu_week_monday_date_to
+        from customer_journey_reactivations
+
+)
+
+select * from reactivations_with_valid_from_and_to
