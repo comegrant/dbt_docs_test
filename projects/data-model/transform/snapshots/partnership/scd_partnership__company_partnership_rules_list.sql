27bf356746286e1555b91c53a88323a55b9bdcee
Grant Magne <77225090+grantlevang@users.noreply.github.com>
2025-08-14 05:27:58 +0200

    Fix partnership rule list snapshot (#1329)

--- projects/data-model/transform/snapshots/partnership/scd_partnership__company_partnership_rules_list.sql
+++ projects/data-model/transform/snapshots/partnership/scd_partnership__company_partnership_rules_list.sql
@@ -5,5 +5,6 @@ select
     , array_sort(collect_list(partnership_rule_id)) as partnership_rule_ids_list
 from {{ source('partnership', 'partnership__company_partnership_rule') }}
 where is_active = true
+group by company_partnership_id
 
 {% endsnapshot %} 
