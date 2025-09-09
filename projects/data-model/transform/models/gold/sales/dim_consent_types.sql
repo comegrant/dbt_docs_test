7778e850c8c923e024a21d9a033fd6db992b1b9e
Marie Borg <72552403+Marieilona@users.noreply.github.com>
2025-09-09 12:08:07 +0200

    add company id to dim consent types (#1479)

--- projects/data-model/transform/models/gold/sales/dim_consent_types.sql
+++ projects/data-model/transform/models/gold/sales/dim_consent_types.sql
@@ -20,6 +20,7 @@ consents as (
         , consents.consent_name
         , consents.consent_id
         , consents.consent_category_id
+        , consents.company_id
     from consents
     left join consent_categories
         on consents.consent_category_id = consent_categories.consent_category_id
