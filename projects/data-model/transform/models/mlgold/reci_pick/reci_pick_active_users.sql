6a7e0c9c1b6a1eb2ca383421b5ca5ee35a3f4c19
Sylvia Liu <102959222+sylvia-liu-qinghua@users.noreply.github.com>
2025-09-09 13:39:32 +0200

    add consent data into reci-pick training and predict data models (#1478)

--- projects/data-model/transform/models/mlgold/reci_pick/reci_pick_active_users.sql
+++ projects/data-model/transform/models/mlgold/reci_pick/reci_pick_active_users.sql
@@ -18,6 +18,45 @@ with dim_preference_combinations as (
     where is_current = true
 )
 
+, fact_billing_agreement_consents as (
+    select
+        billing_agreement_id
+        , is_accepted_consent
+        , fk_dim_consent_types
+        , valid_from
+    from {{ ref('fact_billing_agreement_consents') }}
+)
+
+, consent_types as (
+    select
+        pk_dim_consent_types
+        , consent_category_id
+    from {{ ref('dim_consent_types') }}
+)
+
+, data_processing_consents as (
+    select
+        fact_billing_agreement_consents.*
+    from fact_billing_agreement_consents
+    left join consent_types
+        on fact_billing_agreement_consents.fk_dim_consent_types = consent_types.pk_dim_consent_types
+    where consent_types.consent_category_id = '3495C28B-703C-44AA-B6E0-E01D46684261' -- data processing
+)
+
+, consents_with_row_number as (
+    select
+        *,
+        row_number() over(partition by billing_agreement_id order by valid_from desc) as consent_row_number
+    from data_processing_consents
+)
+
+, latest_consent as (
+    select
+        *
+    from consents_with_row_number
+    where consent_row_number = 1
+)
+
 , dim_companies as (
     select
         pk_dim_companies
@@ -40,10 +79,12 @@ with dim_preference_combinations as (
             = dim_preference_combinations.pk_dim_preference_combinations
     left join dim_companies
         on dim_billing_agreements.company_id = dim_companies.pk_dim_companies
+    left join latest_consent
+        on dim_billing_agreements.billing_agreement_id = latest_consent.billing_agreement_id
     where
         billing_agreement_status_name = 'Active'
         and concept_combinations != 'No concept preferences'
-
+        and latest_consent.is_accepted_consent = true
 )
 
 select * from final
--- projects/data-model/transform/models/mlgold/reci_pick/reci_pick_order_history.sql
+++ projects/data-model/transform/models/mlgold/reci_pick/reci_pick_order_history.sql
@@ -30,6 +30,45 @@ with finished_orders as (
     where is_current = true
 )
 
+, fact_billing_agreement_consents as (
+    select
+        billing_agreement_id
+        , is_accepted_consent
+        , fk_dim_consent_types
+        , valid_from
+    from {{ ref('fact_billing_agreement_consents') }}
+)
+
+, consent_types as (
+    select
+        pk_dim_consent_types
+        , consent_category_id
+    from {{ ref('dim_consent_types') }}
+)
+
+, data_processing_consents as (
+    select
+        fact_billing_agreement_consents.*
+    from fact_billing_agreement_consents
+    left join consent_types
+        on fact_billing_agreement_consents.fk_dim_consent_types = consent_types.pk_dim_consent_types
+    where consent_types.consent_category_id = '3495C28B-703C-44AA-B6E0-E01D46684261' -- data processing
+)
+
+, consents_with_row_number as (
+    select
+        *,
+        row_number() over(partition by billing_agreement_id order by valid_from desc) as consent_row_number
+    from data_processing_consents
+)
+
+, latest_consent as (
+    select
+        *
+    from consents_with_row_number
+    where consent_row_number = 1
+)
+
 , dim_preference_combinations as (
     select
         pk_dim_preference_combinations
@@ -79,6 +118,8 @@ with finished_orders as (
         on
             dim_billing_agreements.preference_combination_id
             = dim_preference_combinations.pk_dim_preference_combinations
+    left join latest_consent
+        on dim_billing_agreements.billing_agreement_id = latest_consent.billing_agreement_id
     where
         is_dish = true
         and ((is_removed_dish = 0) or (is_removed_dish is null))
@@ -86,6 +127,7 @@ with finished_orders as (
         and billing_agreement_status_name != 'Deleted'
         and concept_combinations != 'No concept preferences'
         and main_recipe_id != 0
+        and latest_consent.is_accepted_consent = true
 )
 
 select * from final
