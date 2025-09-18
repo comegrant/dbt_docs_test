fe0ce915ca89241288da2ff4325dac5d2d764b89
abraaum <91672670+abraaum@users.noreply.github.com>
2025-09-18 15:12:31 +0200

    Updated data to datalake (#1536)

--- projects/menu-optimiser/jobs/mop_databricks_to_azure.py
+++ projects/menu-optimiser/jobs/mop_databricks_to_azure.py
@@ -5,6 +5,8 @@ from typing import Literal
 import pandas as pd
 from pydantic import BaseModel, Field
 from pydantic_argparser import parse_args
+from key_vault import key_vault
+from key_vault.interface import KeyVaultInterface
 
 from catalog_connector import connection
 from data_contracts.sources import data_science_data_lake
@@ -14,7 +16,6 @@ class Args(BaseModel):
     environment: Literal["prod", "test", "dev"] = Field("dev")
 
 
-# TODO:make it a dbt model
 def get_carbs_data() -> pd.DataFrame:
     df = connection.sql("""
     select distinct
@@ -38,7 +39,6 @@ def get_carbs_data() -> pd.DataFrame:
     return df
 
 
-# TODO:make it a dbt model
 def get_protein_data() -> pd.DataFrame:
     df = connection.sql("""
     select distinct
@@ -86,6 +86,138 @@ def get_ingredients_data() -> pd.DataFrame:
     return df
 
 
+def get_customer_data() -> pd.DataFrame:
+    df = connection.sql("""
+    with
+
+    agreements as (
+
+        select * from gold.dim_billing_agreements
+        where is_current = True
+            and billing_agreement_status_name = 'Active'
+
+    )
+
+    , preferences as (
+
+        select * from intermediate.int_billing_agreement_preferences_unioned
+
+    )
+
+    , preference_combinations as (
+
+        select * from gold.dim_preference_combinations
+
+    )
+
+    , products as (
+
+        select * from gold.dim_products
+        where product_type_id = '2F163D69-8AC1-6E0C-8793-FF0000804EB3'
+
+    )
+
+    , bridge_agreement_products as (
+
+        select * from gold.bridge_billing_agreements_basket_products
+    )
+
+    , billing_agreement_portions as (
+
+        select
+            agreements.billing_agreement_id
+            , products.portions
+        from agreements
+        left join bridge_agreement_products
+            on agreements.pk_dim_billing_agreements = bridge_agreement_products.fk_dim_billing_agreements
+        left join products
+            on bridge_agreement_products.fk_dim_products = products.pk_dim_products
+
+    )
+
+    , billing_agreement_preferences as (
+
+        select
+            agreements.company_id
+            , agreements.billing_agreement_id
+            , agreements.billing_agreement_status_name
+            , billing_agreement_portions.portions
+            , preference_combinations.concept_name_combinations
+            , preference_combinations.concept_preference_id_list
+            , preference_combinations.taste_name_combinations_including_allergens
+            , preference_combinations.taste_preferences_including_allergens_id_list
+        from agreements
+        left join billing_agreement_portions
+            on agreements.billing_agreement_id = billing_agreement_portions.billing_agreement_id
+        left join preferences
+            on
+                agreements.billing_agreement_preferences_updated_id
+                = preferences.billing_agreement_preferences_updated_id
+        left join preference_combinations
+            on
+                preferences.preference_combination_id
+                = preference_combinations.pk_dim_preference_combinations
+        where preference_combinations.taste_preferences_including_allergens_id_list is not null
+    )
+
+    , agreement_preferences_with_traits as (
+        select
+            bap.company_id
+        , md5(concat_ws('-', bap.company_id, bap.taste_preferences_including_allergens_id_list, bap.concept_preference_id_list))
+            as negative_taste_preference_combo_id
+        , lower(bap.taste_name_combinations_including_allergens) as negative_taste_preferences
+        , bap.taste_preferences_including_allergens_id_list      as negative_taste_preferences_ids
+        , lower(bap.concept_name_combinations)                   as positive_taste_preferences
+        , bap.concept_preference_id_list                         as positive_taste_preferences_ids
+        , bap.billing_agreement_id
+        , bap.portions
+        , bat.customer_journey_sub_segment_name
+        from billing_agreement_preferences bap
+        left join diamond.billing_agreement_traits bat
+        on bap.billing_agreement_id = bat.billing_agreement_id
+    )
+
+    , agreement_preferences_aggregated as (
+        select
+            company_id
+        , negative_taste_preference_combo_id
+        , negative_taste_preferences
+        , negative_taste_preferences_ids
+        , positive_taste_preferences
+        , positive_taste_preferences_ids
+        , count(distinct billing_agreement_id) as number_of_users
+        , count(distinct case when portions = 1 then billing_agreement_id end) as users_with_1_portions
+        , count(distinct case when portions = 2 then billing_agreement_id end) as users_with_2_portions
+        , count(distinct case when portions = 3 then billing_agreement_id end) as users_with_3_portions
+        , count(distinct case when portions = 4 then billing_agreement_id end) as users_with_4_portions
+        , count(distinct case when portions = 5 then billing_agreement_id end) as users_with_5_portions
+        , count(distinct case when portions = 6 then billing_agreement_id end) as users_with_6_portions
+        , count(distinct case when customer_journey_sub_segment_name = 'Churned' then billing_agreement_id end) as users_churned
+        , count(distinct case when customer_journey_sub_segment_name = 'Reactivated' then billing_agreement_id end) as users_reactivated
+        , count(distinct case when customer_journey_sub_segment_name = 'Always On' then billing_agreement_id end) as users_always
+        , count(distinct case when customer_journey_sub_segment_name = 'Frequent' then billing_agreement_id end) as users_frequent
+        , count(distinct case when customer_journey_sub_segment_name = 'Regular' then billing_agreement_id end) as users_regular
+        , count(distinct case when customer_journey_sub_segment_name = 'Occasional' then billing_agreement_id end) as users_occasional
+        , count(distinct case when customer_journey_sub_segment_name = 'Onboarding' then billing_agreement_id end) as users_onboarding
+        , count(distinct case when customer_journey_sub_segment_name = 'Waiting for Onboarding' then billing_agreement_id end) as users_waiting
+        , count(distinct case when customer_journey_sub_segment_name = 'Partial Signup' then billing_agreement_id end) as users_partial
+        , count(distinct case when customer_journey_sub_segment_name = 'Regret' then billing_agreement_id end) as users_regret
+        , count(distinct case when customer_journey_sub_segment_name = 'Unknown' then billing_agreement_id end) as users_unknown
+        from agreement_preferences_with_traits
+        group by
+            company_id
+            , negative_taste_preference_combo_id
+            , negative_taste_preferences
+            , negative_taste_preferences_ids
+            , positive_taste_preferences
+            , positive_taste_preferences_ids
+    )
+
+    select * from agreement_preferences_aggregated;
+    """).toPandas()
+    return df
+
+
 async def write_to_data_lake(df: pd.DataFrame, filename: str, env: str):
     """Write dataframe to azure data lake."""
     df_clean = df.copy()
@@ -97,10 +229,19 @@ async def write_to_data_lake(df: pd.DataFrame, filename: str, env: str):
     )
 
 
-async def main(args: Args) -> None:
+async def main(args: Args, vault: KeyVaultInterface | None = None) -> None:
+    if vault is None:
+        vault = key_vault(env=args.environment)
+
+    await vault.load_env_keys(
+        ["DATALAKE_SERVICE_ACCOUNT_NAME", "DATALAKE_STORAGE_ACCOUNT_KEY"]
+    )
+
     df_carbs = get_carbs_data()
     df_protein = get_protein_data()
     df_ingredients = get_ingredients_data()
+    df_customer = get_customer_data()
+
     await write_to_data_lake(
         df=df_carbs,
         filename="recipe_carbohydrates.parquet",
@@ -116,6 +257,12 @@ async def main(args: Args) -> None:
     await write_to_data_lake(
         df=df_ingredients,
         filename="recipe_ingredients.parquet",
+        env=args.environment,
+    )
+
+    await write_to_data_lake(
+        df=df_customer,
+        filename="customer_info.parquet",
         env=args.environment,
     )
 
